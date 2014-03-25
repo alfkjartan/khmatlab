049e73c63e12b788744bc3a6f6cd541dc05272fe
alfkjartan <kjartan.halvorsen@gmail.com>
2014-03-25 14:37:28 +0100

    Deleted unnecessary files

--- signal/normalize/install.bat
+++
@@ -1,5 +0,0 @@
-mkdir c:\matlabruntime
-mkdir c:\matlabruntime\bin
-copy normalize.exe c:\matlabruntime
-copy norm.bat c:\matlabruntime
-copy bin c:\matlabruntime\bin
--- signal/normalize/norm.bat
+++
@@ -1,2 +0,0 @@
-set PATH=%path%;c:\matlabruntime\bin\win32;c:\matlabruntim\normalize;c:\matlabruntime\normalize\bin
-normalize
